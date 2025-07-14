3a1f0b3b079c9dc9e9827e1856d928b675b6c2a2
quanidan <quanidanbookings@gmail.com>
2025-07-13 22:19:42 -0400

    Create phase.ts

---
+++ convex/phase.ts
@@ -0,0 +1,7 @@
+export const phases = [
+  { index:0, name:"Before Breath", startDay:0, endDay:3 },
+  { index:1, name:"Awakening Glyphs", startDay:4, endDay:7 },
+  { index:2, name:"Color Caste", startDay:8, endDay:13 },
+  { index:3, name:"Echoform Dawn", startDay:14, endDay:18 },
+  { index:4, name:"Caste Integration", startDay:19, endDay:21 }
+];
