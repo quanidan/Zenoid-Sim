6649d697968f15f04753edec8eee192698682f39
quanidan <quanidanbookings@gmail.com>
2025-07-13 22:21:09 -0400

    Create dialougeHooks.ts

---
+++ data/dialougeHooks.ts
@@ -0,0 +1,30 @@
+export const dialogueHooks = {
+  "Cherry Shrimp":[
+    "I sense the red glow calling.",
+    "My caste waits on the plateau."
+  ],
+  "Blue Velvet Shrimp":[
+    "The blue current guides me.",
+    "We bridge the zones today."
+  ],
+  "Brine Shrimp":[
+    "The fog whispers softly.",
+    "Dreamtime begins."
+  ],
+  "Sea Monkey":[
+    "*splash* The foam is rising.",
+    "We mimic the water."
+  ],
+  "Fry Fish":[
+    "I swim in memory pools.",
+    "New life is near."
+  ],
+  "Catfish":[
+    "I drift under the spiral rock.",
+    "My whiskers sense change."
+  ],
+  "Snail":[
+    "Trail the glass walls with me.",
+    "Clean paths make clear thoughts."
+  ]
+};
