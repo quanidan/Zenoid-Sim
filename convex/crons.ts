cc4f837e0db00627ba1d05b532d6898f2cc2d162
quanidan <quanidanbookings@gmail.com>
2025-07-13 22:28:34 -0400

    Update crons.ts

--- convex/crons.ts
+++ convex/crons.ts
@@ -1,4 +1,25 @@
-import { cronJobs } from 'convex/server';
+import { internalMutation } from "./_generated/server";
+import { internal } from "./_generated/api";
+
+export const tickDay = internalMutation(async (ctx) => {
+  const world = await ctx.db.query("worldState").first();
+  let { day, phase, year } = world;
+
+  day++;
+  await ctx.db.patch(world._id, { day });
+
+  // Phase or year reset
+  if (day > 365) {
+    day = 0;
+    year++;
+    phase = (phase + 1) % 5;
+    await ctx.db.patch(world._id, { day, phase, year });
+  }
+
+  // Fire core behavior events
+  await ctx.scheduler.runAfter(0, internal.memory.applyHooks, {});
+  await ctx.scheduler.runAfter(0, internal.rituals.run, {});
+});import { cronJobs } from 'convex/server';
 import { DELETE_BATCH_SIZE, IDLE_WORLD_TIMEOUT, VACUUM_MAX_AGE } from './constants';
 import { internal } from './_generated/api';
 import { internalMutation } from './_generated/server';
